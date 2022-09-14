9aaf5cced12ce813ac705441a0db58568c289689
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-14 14:45:39 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+  get '/auth/google_oauth2/callback', to: 'users#create'
+  resource :users, only: [:show] do
+    get '/dashboard', to: "users#show"
+  end
+end
