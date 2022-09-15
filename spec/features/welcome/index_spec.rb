3a088b7d667eb602ba525af6aca92b111841dbfd
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-15 11:43:17 -0600

    test/Add additional expects to welcome index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe 'landing page' do
   describe '#index' do
     it 'displays the title of the application' do
+
       visit root_path
 
       expect(page).to have_content("Messages from the Stars")
+      expect(page).to have_content("Catch a satellite")
+      expect(page).to have_content("Create a message")
+      expect(page).to have_content("Cast your message into the cosmic ocean")
+      expect(page).to have_content("Log in via Google to begin exploring the skies")
     end
   end
 end
