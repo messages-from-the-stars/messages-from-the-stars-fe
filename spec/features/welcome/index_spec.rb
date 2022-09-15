2ed96f1af6c27afd2719dd323b36aee41b98f9d6
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-15 12:11:37 -0600

    test/Add sad path test when I user puts in invalid credentials

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -12,22 +12,24 @@ RSpec.describe 'landing page' do
       expect(page).to have_content("Cast your message into the cosmic ocean")
       expect(page).to have_content("Log in via Google to begin exploring the skies")
     end
+  end
 
+  describe '#happy path' do
     it 'has button to log in user, click button to authorize log in via Google' do
       OmniAuth.config.test_mode = true
-        OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-          {"provider" => "google_oauth2",
-            "uid" => "10000000000000000",
-            "info" => {
-              "name" => "John Smith",
-              "email" => "john@example.com",
-              "first_name" => "John",
-              "last_name" => "Smith",
-            },
-            "credentials" => {
-              "token" => "Token",
-            },
-          })
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+        {"provider" => "google_oauth2",
+          "uid" => "10000000000000000",
+          "info" => {
+            "name" => "John Smith",
+            "email" => "john@example.com",
+            "first_name" => "John",
+            "last_name" => "Smith",
+          },
+          "credentials" => {
+            "token" => "Token",
+          },
+        })
 
       visit root_path
 
@@ -36,6 +38,32 @@ RSpec.describe 'landing page' do
       click_link('Log In')
 
       expect(current_path).to eq(dashboard_users_path)
+    end
+  end
+
+  describe '#sad_path' do
+    it 'Displays Invalid credentials if log in fails' do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google_oauth2] =     OmniAuth::AuthHash.new(
+        {"provider" => "google_oauth2",
+          "uid" => "10000000000000000",
+          "info" => {
+            "name" => "John Smith",
+            "email" => "john@example.com",
+            "first_name" => "John",
+            "last_name" => "Smith",
+          },
+          "credentials" => {
+            "token" => "",
+          },
+        })
+
+      visit root_path
+
+      click_link('Log In')
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('Invalid Credentials')
     end
   end
 end
