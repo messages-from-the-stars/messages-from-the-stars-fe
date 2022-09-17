70d96f43cbc98896342399c52b0e9173066b9c39
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-17 17:04:55 -0400

    Feat: show page displays message content

--- app/facades/message_facade.rb
+++ app/facades/message_facade.rb
@@ -2,6 +2,8 @@ class MessageFacade
 
   def self.get_message(message_id)
     json = MessageService.get_message(message_id)
+    
+    Message.new(json)
   end
 
 end
--- spec/features/messages/show_spec.rb
+++ spec/features/messages/show_spec.rb
@@ -14,10 +14,12 @@ RSpec.describe 'message show page' do
     @message_content = @message_call[:data][:attributes][:content]
 
     @message_created_at = @message_call[:data][:attributes][:created_at]
+
+    allow(MessageService).to receive(:get_message).and_return(@message_call)
   end
 
   it 'displays message content' do
-    visit "/api/v1/messages/#{@message_id}"
+    visit "/api/v1/messages/#{@message_id}" 
 
     expect(page).to have_content("Message: #{@message_content}")
   end
