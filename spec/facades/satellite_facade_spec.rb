6e22552e8f0783791457cdf201264586f94c16f1
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-17 10:23:53 -0600

    test: added test for user satellites to unskip once be is hosted

--- spec/facades/satellite_facade_spec.rb
+++ spec/facades/satellite_facade_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ' SatelliteFacade' do
-  it 'returns visible times when a satellite passes overhead', :vcr do
-     sat_visibility =  SatelliteFacade.get_satellite_visibility(39847, 39.6431, -104.8987)
+    it 'returns visible times when a satellite passes overhead', :vcr do
+        sat_visibility =  SatelliteFacade.get_satellite_visibility(39847, 39.6431, -104.8987)
 
-    expect(sat_visibility).to be_a(Array)
-    expect(sat_visibility).to be_all(SatelliteVisibility)
-  end
+        expect(sat_visibility).to be_a(Array)
+        expect(sat_visibility).to be_all(SatelliteVisibility)
+    end
+
+    xit 'returns visible times when a satellite passes overhead', :vcr do
+        satellites =  SatelliteService.get_user_satellites("123")
+
+        expect(satellites).to be_a(Array)
+        expect(satellites).to be_all(Satellite)
+    end
 end
