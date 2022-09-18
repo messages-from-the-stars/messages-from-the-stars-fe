0a598d0c83543f348ffa699cda879336d7213c0a
Thiago Silveira <thiago1992@yahoo.com>
2022-09-18 13:41:44 -0600

    Feature: Discover controller index with message_count functionality
    Co-authored-by: Michael Koulouvaris <mikekoul@gmail.com>

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -3,6 +3,9 @@ class DiscoverController < ApplicationController
 
   def index
     @satellites = SatelliteFacade.above_satellites(@lat, @long)
+    @message_count = @satellites.map do |satellite|
+      SatelliteFacade.get_sat_message_id(satellite.satid)
+    end.flatten
   end
 
 end
