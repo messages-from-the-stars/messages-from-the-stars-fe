188863fbf33cc577eaf71cfceec1bb5180a581eb
Thiago Silveira <thiago1992@yahoo.com>
2022-09-17 21:50:15 -0600

    Test: Satellite message poro spec
    Co-authored-by: Michael Koulouvaris <mikekoul@gmail.com>

---
+++ spec/poros/satellite_message_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe SatelliteMessage do
+
+  it 'exists' do
+    data = {
+            :id=>"2",
+            :type=>"message",
+            :attributes=>
+              {:satellite_id=>19,
+              :start_lat=>40.57418114605389,
+              :start_lng=>32.18080704424722,
+              :content=>"What a piece of work is man!",
+              :created_at=>"2022-09-12T18:42:01.000Z",
+              :updated_at=>"2022-04-05T22:43:05.000Z"
+            }
+        
+    }
+
+    message = SatelliteMessage.new(data)
+
+    expect(message).to be_a SatelliteMessage
+    expect(message.id).to eq(2)
+    expect(message.satellite_id).to eq(19)
+    expect(message.start_lat.round(2)).to eq(40.57)
+    expect(message.start_lng.round(2)).to eq(32.18)
+    expect(message.content).to eq("What a piece of work is man!")
+    expect(message.created_at).to eq("2022-09-12T18:42:01.000Z")
+  end
+
+end
