343577733cdcc0ccaef05b7fbeaaaed2b33386da
philmarcu <philmark18@gmail.com>
2022-09-19 15:23:14 -0600

    Feat - add passing test for sat_map_maker poro

---
+++ app/poros/sat_map_maker.rb
@@ -0,0 +1,22 @@
+class SatMapMaker
+  def initialize(sat_lat, sat_lng)
+    @sat_lat =  sat_lat
+    @sat_lng =  sat_lng
+  end
+
+  def result_url
+    "http://maps.googleapis.com/maps/api/staticmap?" + image_style + sat_marker_params + api_key
+  end
+
+  def image_style
+    "&size=600x400&style=visibility:on&maptype=satellite"
+  end
+
+  def sat_marker_params
+    "&markers=size:medium|color:red|label:S|#{@sat_lat},#{@sat_lng}"
+  end
+
+  def api_key
+    "&key=#{ENV['google_maps_key']}"
+  end
+end
---
+++ spec/poros/sat_map_maker_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe SatMapMaker do
+  
+  before :each do
+    @sat_lat = 38.9072
+    @sat_lng = -77.0369
+
+    @sat_map_maker = SatMapMaker.new(@sat_lat, @sat_lng)
+  end
+
+  it 'exists' do
+    expect(@sat_map_maker).to be_a SatMapMaker
+  end
+
+  it 'returns location parameter string for user' do
+    expect(@sat_map_maker.sat_marker_params).to eq("&markers=size:medium|color:red|label:S|#{@sat_lat},#{@sat_lng}")
+  end
+
+  it 'returns full url' do
+    expect(@sat_map_maker.result_url).to eq("http://maps.googleapis.com/maps/api/staticmap?&size=600x400&style=visibility:on&maptype=satellite&markers=size:medium|color:red|label:S|38.9072,-77.0369&key=#{ENV['google_maps_key']}")
+  end
+
+end
