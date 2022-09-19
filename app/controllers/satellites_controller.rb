2ee324b1fe84569570e3af4dd39658835642e5a7
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-19 12:46:13 -0600

    ref: messages display on satellite show page

--- app/controllers/satellites_controller.rb
+++ app/controllers/satellites_controller.rb
@@ -5,7 +5,7 @@ class SatellitesController < ApplicationController
     @satellite = SatelliteFacade.get_satellite_position(params[:sat_id])
     @visibility = SatelliteFacade.get_satellite_visibility(params[:sat_id], @lat, @long)
     @weather_forecasts = WeatherFacade.get_weather_forecast(@lat, @long)
-    @messages = SatelliteFacade.get_sat_message_id(params[:sat_id])
     @sat_id = SatelliteFacade.create_user_satellite(params[:sat_id], session[:user_id])
+    @messages = SatelliteFacade.get_sat_message_id(@sat_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+  
   get '/auth/google_oauth2/callback', to: 'users#create'
+
   get '/satellite', to: 'satellites#show'
+
   resource :users do
     get '/dashboard', to: "users#show"
     get '/discover', to: "discover#index"
@@ -10,11 +13,6 @@ Rails.application.routes.draw do
   end
   
   resources :messages, only: [:show, :new]
+
   post 'messages/create', to: 'messages#create'
-  
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :satellites, only: [:show]
-  #   end
-  # end
 end
