af324652a1cea607e2f8fc78fc9a14b96450777e
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-19 17:52:38 -0400

    Feat: message travel time

--- app/poros/message.rb
+++ app/poros/message.rb
@@ -16,4 +16,17 @@ class Message
     @created_at = data[:data].first[:attributes][:created_at]
   end
 
+  def formatted_creation_date
+    datetime = DateTime.parse(@created_at)
+
+    datetime.strftime("%d %B %Y, %l:%M %p")
+  end
+
+  def travel_time_days
+    datetime = DateTime.parse(@created_at)
+    today = DateTime.now
+
+    (today - datetime).to_i
+  end
+
 end
