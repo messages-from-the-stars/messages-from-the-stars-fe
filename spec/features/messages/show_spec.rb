063b6e82c4f02a49fa0ad438664a62f5ae8c0c9f
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-20 15:41:09 -0600

    update message show page tests to work with required user method

--- spec/features/messages/show_spec.rb
+++ spec/features/messages/show_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'message show page' do
   before :each do
+     OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      {"provider" => "google_oauth2",
+          "uid" => "10000000000000000",
+          "info" => {
+          "name" => "John Smith",
+          "email" => "john@example.com",
+          "first_name" => "John",
+          "last_name" => "Smith",
+          },
+          "credentials" => {
+          "token" => "Token",
+          },
+      })
+
     @message_call = JSON.parse(File.read('spec/fixtures/message.json'), symbolize_names: true)
     @sat_db_call = JSON.parse(File.read('spec/fixtures/sat_db_response.json'), symbolize_names: true)
     @sat_position_call = JSON.parse(File.read('spec/fixtures/sat_position_response.json'), symbolize_names: true)
@@ -26,12 +41,19 @@ RSpec.describe 'message show page' do
     @sat_lng = @sat_position_call[:positions].first[:satlongitude]
     @lat = 39.75
     @long = -104.99
-
+    @weather_data = JSON.parse(File.read('spec/fixtures/weather_data.json'), symbolize_names: true)
+    @satellites = JSON.parse(File.read('spec/fixtures/satellites.json'), symbolize_names: true)
+    @visible_sat_times = JSON.parse(File.read('spec/fixtures/satellite_visibility.json'), symbolize_names: true)
+    
     allow_any_instance_of(ApplicationController).to receive(:remote_ip).and_return(@lat, @long)
     allow(MessageService).to receive(:get_message).and_return(@message_call)
     allow(SatelliteService).to receive(:get_norad_id).and_return(@sat_db_call)
     allow(SatelliteService).to receive(:get_satellite_position).and_return(@sat_position_call)
+    allow(WeatherService).to receive(:get_weather_forecast).and_return(@weather_data)
+    allow(SatelliteService).to receive(:get_user_satellites).and_return(@satellites)
+    allow(SatelliteService).to receive(:get_satellite_visibility).and_return(@visible_sat_times)
 
+    visit '/auth/google_oauth2'
   end
 
   it 'displays message content', :vcr do
