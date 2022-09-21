83c68ae95dc413e5725433cab77017bdba0125b9
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-21 12:59:02 -0600

    ref: update message facade tests

--- spec/facades/message_facade_spec.rb
+++ spec/facades/message_facade_spec.rb
@@ -3,26 +3,15 @@ require 'rails_helper'
 RSpec.describe MessageFacade do
 
   describe "#get_message" do
-
-    it 'returns a message' do
-
-      @message_call = JSON.parse(File.read('spec/fixtures/message.json'), symbolize_names: true)
-
-      allow(MessageService).to receive(:get_message).and_return(@message_call)
-
-      message_id = @message_call[:data][:id]
-
-      results = MessageFacade.get_message(message_id)
+    it 'returns a message', :vcr do 
+      results = MessageFacade.get_message(96)
 
       expect(results).to be_a Message
-
     end
-
   end
 
   describe "#create_message" do
-
-    it 'creates a message' do
+    it 'creates a message' do 
       response = {status: 200}
       lat =  40.7143
       long = -74.006
@@ -30,12 +19,18 @@ RSpec.describe MessageFacade do
       message = "Hello there, this is a test"
 
       allow(MessageService).to receive(:create_message).and_return(response)
-
       message = MessageService.create_message(lat, long, message, sat_id)
 
       expect(message[:status]).to eq(200)
     end
+  end
 
+  describe "#get_message_count" do
+    it 'returns a message count', :vcr do
+      results = MessageFacade.get_message_count(22195)
+
+      expect(results).to be_a Array 
+    end
   end
 
 end
