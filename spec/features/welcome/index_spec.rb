0ec03a86eabfb4519c15eaae79ca499fd5b8c291
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-21 12:42:03 -0600

    feat: update welcome index feature tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -16,32 +16,23 @@ RSpec.describe 'landing page' do
 
   describe '#happy path' do
     it 'has button to log in user, click button to authorize log in via Google' do
-      OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-        {"provider" => "google_oauth2",
-          "uid" => "10000000000000000",
-          "info" => {
-            "name" => "John Smith",
-            "email" => "john@example.com",
-            "first_name" => "John",
-            "last_name" => "Smith",
-          },
-          "credentials" => {
-            "token" => "Token",
-          },
-        })
-      
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+            
+      @found_satellites = JSON.parse(File.read('spec/fixtures/above_satellites.json'), symbolize_names: true)
       @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
       @satellites = JSON.parse(File.read('spec/fixtures/satellites.json'), symbolize_names: true)
       @visible_sat_times = JSON.parse(File.read('spec/fixtures/satellite_visibility.json'), symbolize_names: true)
       @weather_data = JSON.parse(File.read('spec/fixtures/weather_data.json'), symbolize_names: true)
+      @messages = JSON.parse(File.read('spec/fixtures/messages.json'), symbolize_names: true)
       @lat = 39.75
       @long = -104.99
-
+      
       allow(SatelliteService).to receive(:get_user_satellites).and_return(@satellites)
       allow(SatelliteService).to receive(:get_satellite_visibility).and_return(@visible_sat_times)
       allow(WeatherService).to receive(:get_weather_forecast).and_return(@weather_data)
       allow(UserService).to receive(:find_or_create_user).and_return(@user)
+      allow(SatelliteService).to receive(:get_satellites_in_range).and_return(@found_satellites)
+      allow(SatelliteService).to receive(:get_sat_message).and_return(@messages)
       allow_any_instance_of(ApplicationController).to receive(:remote_ip).and_return(@lat, @long)
 
       visit root_path
